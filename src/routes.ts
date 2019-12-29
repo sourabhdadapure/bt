cb37b7d5b5b4dfba17b75a0d6eb9512616d95798
sourabhdadapure <sourabhdadapure@gmail.com>
2019-12-29 15:48:49 -0500

    removes unused imports

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Home from './screens/Home';
