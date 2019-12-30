6084d6e76b9c48fd7a8cfde42e202452438ced7b
sourabhdadapure <sourabhdadapure@gmail.com>
2019-12-30 01:20:59 -0500

    Adds navigation button

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
-import * as React from 'react';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import Home from './screens/Home';
+import GeoLocation from './screens/GeoLocation';
 
 const AppNavigator = createStackNavigator({
   Home: {
     screen: Home,
+  },
+  GeoLocation: {
+    screen: GeoLocation,
   },
 });
 
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 
 export default class Home extends React.Component {
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Home Screen</Text>
+        <TouchableOpacity style={{backgroundColor: 'red'}}>
+          <Text style={{padding: 20, borderRadius: 10}}>GeoLocation </Text>
+        </TouchableOpacity>
       </View>
     );
   }
